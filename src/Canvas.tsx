e99aab8a13bf097f836b4d3dcdf97a88acee7f3d
Daniel <info@daniel-bird.co.uk>
2021-10-12 15:58:38 +0100

    Use Recoil over context to reduce amount of rerendering on state change

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,18 +1,34 @@
-import {PageContainer} from './PageContainer'
+import {atom, useRecoilValue, useSetRecoilState} from 'recoil'
 import {Rectangle} from './components/Rectangle/Rectangle'
+import {PageContainer} from './PageContainer'
 import {Toolbar} from './Toolbar'
 
+export const selectedElementState = atom<number | null>({
+  key: 'selectedElement',
+  default: null,
+})
+
+export const elementsState = atom<number[]>({
+  key: 'elements',
+  default: [],
+})
+
 function Canvas() {
-    return (
-        <PageContainer
-            onClick={() => {
-                console.log('Deselect all elements!')
-            }}
-        >
-            <Toolbar />
-            <Rectangle />
-        </PageContainer>
-    )
+  const setSelectedElement = useSetRecoilState(selectedElementState)
+  const elements = useRecoilValue(elementsState)
+
+  return (
+    <PageContainer
+      onClick={() => {
+        setSelectedElement(null)
+      }}
+    >
+      <Toolbar />
+      {elements.map((id) => (
+        <Rectangle key={id} id={id} />
+      ))}
+    </PageContainer>
+  )
 }
 
 export default Canvas
