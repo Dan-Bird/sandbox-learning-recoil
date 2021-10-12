efd562fd28f02fd5652550dc1f4e34df4c464400
Daniel <info@daniel-bird.co.uk>
2021-10-12 14:54:42 +0100

    Complete Atom example

---
+++ src/examples/Atoms.tsx
@@ -0,0 +1,49 @@
+import {atom, useRecoilState, useRecoilValue} from 'recoil'
+
+const darkModeAtom = atom({
+    key: 'darkMode',
+    default: false,
+})
+
+const DarkModeSwitch = () => {
+    const [darkMode, setDarkMode] = useRecoilState(darkModeAtom)
+    console.log(`darkMode`, darkMode)
+
+    return (
+        <input
+            type="checkbox"
+            checked={darkMode}
+            onChange={(event) => setDarkMode(event?.currentTarget.checked)}
+        />
+    )
+}
+
+const Button = () => {
+    const darkMode = useRecoilValue(darkModeAtom)
+
+    return (
+        <button
+            style={{
+                backgroundColor: darkMode ? 'black' : 'white',
+                color: darkMode ? 'white' : 'black',
+            }}
+        >
+            My UI Button
+        </button>
+    )
+}
+
+const Atoms = () => {
+    return (
+        <div>
+            <div>
+                <DarkModeSwitch />
+            </div>
+            <div>
+                <Button />
+            </div>
+        </div>
+    )
+}
+
+export default Atoms
