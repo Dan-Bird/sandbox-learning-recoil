da1727e7a2d06ec1158b60916fcb9c822b219f33
Daniel <info@daniel-bird.co.uk>
2021-10-13 11:28:17 +0100

    Use Async selectors for fetching data

--- src/index.tsx
+++ src/index.tsx
@@ -7,26 +7,30 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import {RecoilRoot} from 'recoil'
 import Atoms from './examples/Atoms'
 import {Selectors} from './examples/Selectors'
+import {Async} from './examples/Async'
 
 ReactDOM.render(
-    <React.StrictMode>
-        <RecoilRoot>
-            <ChakraProvider>
-                <Router>
-                    <Switch>
-                        <Route path="/examples/atoms">
-                            <Atoms />
-                        </Route>
-                        <Route path="/examples/selectors">
-                            <Selectors />
-                        </Route>
-                        <Route>
-                            <Canvas />
-                        </Route>
-                    </Switch>
-                </Router>
-            </ChakraProvider>
-        </RecoilRoot>
-    </React.StrictMode>,
-    document.getElementById('root'),
+  <React.StrictMode>
+    <RecoilRoot>
+      <ChakraProvider>
+        <Router>
+          <Switch>
+            <Route path="/examples/atoms">
+              <Atoms />
+            </Route>
+            <Route path="/examples/selectors">
+              <Selectors />
+            </Route>
+            <Route path="/examples/async">
+              <Async />
+            </Route>
+            <Route>
+              <Canvas />
+            </Route>
+          </Switch>
+        </Router>
+      </ChakraProvider>
+    </RecoilRoot>
+  </React.StrictMode>,
+  document.getElementById('root'),
 )
